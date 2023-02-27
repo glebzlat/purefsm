8993a64a64452ebcf13a902ccef4b0b98ab45305
edKotinsky <edkotinsky@gmail.com>
2023-02-27 21:49:58 +0600

    Namespace logger, detect if compiler supports the reflection

--- include/pure/logger.hpp
+++ include/pure/logger.hpp
@@ -5,19 +5,49 @@
 
 namespace pure {
 
-  namespace __details {
+  namespace logger {
+
+#if defined(__GNUC__) || defined(__MINGW32__) || defined(__clang__) ||         \
+    defined(__INTEL_COMPILER) || (defined(__ICC) && (__ICC >= 600)) ||         \
+    (defined(__DMC__) && __DMC__ >= 0x810)
 
     template <typename T>
     std::string type_name() {
+      using size_type = typename std::string::size_type;
       char const* str = __PRETTY_FUNCTION__;
       while (*str != '=') ++str;
       str += 2;
-      std::size_t size = 0;
+      size_type size = 0;
       while (str[size] != ';' && str[size] != ']') ++size;
       return std::string(str, size);
     }
 
-  } // namespace __details
+#elif defined(__MSC_VER) || defined(__FUNCSIG__)
+
+    template <typename T>
+    std::string type_name() {
+      using size_type = typename std::string::size_type;
+      const std::string s = __FUNCSIG__;
+      const std::string fname = "type_name<";
+      const std::string rest = ">(void)";
+      size_type pos = s.find(fname) + fname.size();
+      size_type count = s.find(rest) - pos;
+      return std::string(s, pos, count);
+    }
+
+#else
+
+  #warning                                                                     \
+      "Your compiler does not support the reflection required by the logger"
+
+    template <typename T>
+    std::string type_name() {
+      return {};
+    }
+
+#endif
+
+  } // namespace logger
 
   template <std::ostream& stream>
   class stdout_logger {
@@ -26,7 +56,7 @@ namespace pure {
 
     template <typename T>
     inline void write(const char* str) {
-      stream << str << __details::type_name<T>() << std::endl;
+      stream << str << logger::type_name<T>() << std::endl;
     }
 
     inline void write(const char* str) { stream << str << std::endl; }
@@ -42,7 +72,7 @@ namespace pure {
 
     template <typename T>
     inline void write(const char* str) {
-      stream << str << __details::type_name<T>() << std::endl;
+      stream << str << logger::type_name<T>() << std::endl;
     }
 
     inline void write(const char* str) { stream << str << std::endl; }
